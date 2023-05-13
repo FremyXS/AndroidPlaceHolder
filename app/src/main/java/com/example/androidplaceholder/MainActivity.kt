1d2954da31169ad93eb4c65f20dee40cc58aa476
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-05-13 17:00:26 +0500

    build: add start package

--- app/src/main/java/com/example/androidplaceholder/MainActivity.kt
+++ app/src/main/java/com/example/androidplaceholder/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.androidplaceholder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidplaceholder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var bind : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        bind = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(bind.root)
     }
 }
