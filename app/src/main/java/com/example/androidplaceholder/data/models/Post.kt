634543dcb203ed5bf8b8ce0311a53e9ac8e536fb
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-17 01:51:46 +0500

    refactor: change Post and User models

--- app/src/main/java/com/example/androidplaceholder/data/models/Post.kt
+++ app/src/main/java/com/example/androidplaceholder/data/models/Post.kt
@@ -1,8 +1,24 @@
 package com.example.androidplaceholder.data.models
 
-data class Post(
-    val userId: Int?,
-    val id: Int?,
-    val title: String?,
-    val body: String?,
-)
+sealed class PostDefault(
+    open val userId: Int?,
+    open val id: Int?,
+    open val title: String?,
+    open val body: String?,
+){
+    data class Post(
+        override val userId: Int?,
+        override val id: Int?,
+        override val title: String?,
+        override val body: String?,
+    ) : PostDefault(userId, id, title, body)
+
+    data class PostInfo(
+        override val userId: Int?,
+        override val id: Int?,
+        override val title: String?,
+        override val body: String?,
+        val userFullName: String?,
+        val countComments: Int?
+    ) : PostDefault(userId, id, title, body)
+}
--- app/src/main/java/com/example/androidplaceholder/data/models/User.kt
+++ app/src/main/java/com/example/androidplaceholder/data/models/User.kt
@@ -1,8 +1,15 @@
 package com.example.androidplaceholder.data.models
 
-data class User (
-    val id: Int?,
-    val name: String?,
-    val username: String?,
-    val email: String?,
-)
+sealed class UserDefault (
+    open val id: Int?,
+    open val name: String?,
+    open val username: String?,
+    open val email: String?,
+){
+    data class User(
+        override val id: Int?,
+        override val name: String?,
+        override val username: String?,
+        override val email: String?,
+    ) : UserDefault(id, name, username, email)
+}
