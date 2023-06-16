3d294f4220f8b164ae3b698e06cc93b36326d765
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-16 16:49:55 +0500

    feat: add folder date and models

---
+++ app/src/main/java/com/example/androidplaceholder/data/models/Post.kt
@@ -0,0 +1,8 @@
+package com.example.androidplaceholder.data.models
+
+data class Post(
+    val userId: Int?,
+    val id: Int?,
+    val title: String?,
+    val body: String?,
+)
---
+++ app/src/main/java/com/example/androidplaceholder/data/models/User.kt
@@ -0,0 +1,8 @@
+package com.example.androidplaceholder.data.models
+
+data class User (
+    val id: Int?,
+    val name: String?,
+    val username: String?,
+    val email: String?,
+)
